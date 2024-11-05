76653050940750d28a889b5984438a3d0d43aed9
Hoang-Doan123 <hoangdd.ba12-078@st.usth.edu.vn>
2024-11-05 17:30:08 +0700

    Practical 15

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -2,6 +2,8 @@ package vn.edu.usth.weather;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -24,11 +27,15 @@ import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Objects;
 
 public class WeatherActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+    private ImageView logoImageView;
 
     public static final String TAG = "Weather";
     public static final String NETWORK_RESPONSE = "NETWORK_RESPONSE";
@@ -78,6 +85,18 @@ public class WeatherActivity extends AppCompatActivity {
         // Use ToolBar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        logoImageView = findViewById(R.id.usth_logo);
+
+        if (logoImageView == null) {
+            Log.e(TAG, "logoImageView is null. Check the layout file.");
+        }
+
+        // Start the AsyncTask to download the logo
+        new DownloadImageTask().execute();
+
+        // Start AsyncTask for network request simulation
+        new RequestNetworkTask().execute();
     }
 
     // Inflate the menu
@@ -108,67 +127,133 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
     // Use thread and handler, simulate a network request and show on a toast
-    public void request_network(){
-//        // Handler
-//        final Handler handler = new Handler(Looper.getMainLooper()) {
-//            @Override
-//            public void handleMessage(Message msg) {
-//                // This method is executed in main thread
-//                String content = msg.getData().getString(NETWORK_RESPONSE);
-//                Toast.makeText(WeatherActivity.this, content, Toast.LENGTH_SHORT).show();
-//            }
-//        };
+//    public void request_network(){
+////        // Handler
+////        final Handler handler = new Handler(Looper.getMainLooper()) {
+////            @Override
+////            public void handleMessage(Message msg) {
+////                // This method is executed in main thread
+////                String content = msg.getData().getString(NETWORK_RESPONSE);
+////                Toast.makeText(WeatherActivity.this, content, Toast.LENGTH_SHORT).show();
+////            }
+////        };
+////
+////        // Thread
+////        Thread t = new Thread(new Runnable() {
+////            @Override
+////            public void run() {
+////                // this method is run in a worker thread
+////                try {
+////                    // wait for 1 seconds to simulate a long network access
+////                    Thread.sleep(1000);
+////                } catch (InterruptedException e) {
+////                    e.printStackTrace();
+////                }
+////                // Assume that we got our data from server
+////                Bundle bundle = new Bundle();
+////                bundle.putString(NETWORK_RESPONSE, "Request Network");
+////                // notify main thread
+////                Message msg = new Message();
+////                msg.setData(bundle);
+////                handler.sendMessage(msg);
+////            }
+////        });
+////        t.start();
 //
-//        // Thread
-//        Thread t = new Thread(new Runnable() {
+//        // «Upgrade» Thread/Handler combo with an AsyncTask
+//        AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
+//            // sleep() in doInBackground
 //            @Override
-//            public void run() {
-//                // this method is run in a worker thread
+//            protected String doInBackground(Void... voids) {
 //                try {
 //                    // wait for 1 seconds to simulate a long network access
 //                    Thread.sleep(1000);
 //                } catch (InterruptedException e) {
 //                    e.printStackTrace();
 //                }
-//                // Assume that we got our data from server
-//                Bundle bundle = new Bundle();
-//                bundle.putString(NETWORK_RESPONSE, "Request Network");
-//                // notify main thread
-//                Message msg = new Message();
-//                msg.setData(bundle);
-//                handler.sendMessage(msg);
+//                return "Request Network";
 //            }
-//        });
-//        t.start();
+//
+//            // Toast in onPostExecute()
+//            @Override
+//            protected void onPostExecute(String content) {
+//                Toast.makeText(WeatherActivity.this, content, Toast.LENGTH_SHORT).show();
+//            }
+//        };
+//
+//        task.execute();
+//    }
 
-        // «Upgrade» Thread/Handler combo with an AsyncTask
-        AsyncTask<Void, Void, String> task = new AsyncTask<Void, Void, String>() {
-            // sleep() in doInBackground
-            @Override
-            protected String doInBackground(Void... voids) {
-                try {
-                    // wait for 1 seconds to simulate a long network access
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+    // «Upgrade» the previous AsyncTask
+    private class RequestNetworkTask extends AsyncTask<Void, Void, String> {
+        // sleep() in doInBackground
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                // wait for 1 seconds to simulate a long network access
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "Request Network";
+        }
+
+        // Toast in onPostExecute()
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(WeatherActivity.this, result, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // Perform a real network request to USTH’s server
+    private class DownloadImageTask extends AsyncTask<Void, Void, Bitmap> {
+        // Download USTH logo
+        @Override
+        protected Bitmap doInBackground(Void... voids) {
+            Bitmap bitmap = null;
+            try {
+                // Initialize URL
+                URL url = new URL("https://cdn.haitrieu.com/wp-content/uploads/2022/11/Logo-Truong-Dai-hoc-Khoa-hoc-va-Cong-nghe-Ha-Noi.png");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.setRequestMethod("GET");
+                connection.setDoInput(true);
+                connection.connect();
+
+                // Receive response
+                int response = connection.getResponseCode();
+                Log.i("USTHWeather", "The response is: " + response);
+
+                if (response == HttpURLConnection.HTTP_OK) { // Check if response is OK
+                    InputStream is = connection.getInputStream();
+                    // Process image response
+                    bitmap = BitmapFactory.decodeStream(is);
+                    is.close(); // Close the InputStream
                 }
-                return "Request Network";
-            }
 
-            // Toast in onPostExecute()
-            @Override
-            protected void onPostExecute(String content) {
-                Toast.makeText(WeatherActivity.this, content, Toast.LENGTH_SHORT).show();
+                connection.disconnect();
+            } catch (Exception e) {
+                Log.e("USTHWeather", "Error downloading image: " + e.getMessage());
             }
-        };
+            return bitmap; // Return the bitmap
+        }
 
-        task.execute();
+        // Show it on an ImageView of ForecastFragment
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap != null && logoImageView != null) {
+                logoImageView.setImageBitmap(bitmap); // Set the bitmap to the ImageView
+                Toast.makeText(WeatherActivity.this, "Image Loaded", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(WeatherActivity.this, "Failed to load image", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        request_network();
+        Log.i("Start","Start");
     }
 
     @Override
